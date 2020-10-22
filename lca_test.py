498e1ca88ef9fc8b820068e183e9783c00904c2d
jakslanf <72381232+jakslanf@users.noreply.github.com>
2020-10-22 11:55:31 +0100

    imported unit testing properly

--- lca_test.py
+++ lca_test.py
@@ -1,42 +1,41 @@
 import lca
-
-def testEmpty():
-    root = lca.Node(1)
+import unittest
 
 
-    assert (lca.findLCA(root, 6, 7)) == -1, "Should be -1"
-    assert (lca.findLCA(root, 3, 2)) == -1, "Should be -1"
-    assert (lca.findLCA(root, 4, 5)) == -1, "Should be -1"
-    return
+class TestLCA(unittest.TestCase):
+    def testEmpty(self):
+        root = lca.Node(1)
 
 
-def testNormal():
-    root = lca.Node(1)
-    root.left = lca.Node(2)
-    root.right = lca.Node(3)
-    root.left.left = lca.Node(4)
-    root.left.right = lca.Node(5)
-    root.right.left = lca.Node(6)
-    root.right.right = lca.Node(7)
+        assert (lca.findLCA(root, 6, 7)) == -1, "Should be -1"
+        assert (lca.findLCA(root, 3, 2)) == -1, "Should be -1"
+        assert (lca.findLCA(root, 4, 5)) == -1, "Should be -1"
+        return
 
 
-    assert (lca.findLCA(root, 6, 7)) == 3, "Should be 3"
-    assert (lca.findLCA(root, 3, 2)) == 1, "Should be 1"
-    assert (lca.findLCA(root, 4, 5)) == 2, "Should be 2"
-    return
-
-def testOneMissing():
-    root = lca.Node(1)
-    root.left = lca.Node(2)
-    root.right = lca.Node(3)
-    root.left.left = lca.Node(4)
-    root.left.right = lca.Node(5)
-    root.right.left = lca.Node(6)
+    def testNormal(self):
+        root = lca.Node(1)
+        root.left = lca.Node(2)
+        root.right = lca.Node(3)
+        root.left.left = lca.Node(4)
+        root.left.right = lca.Node(5)
+        root.right.left = lca.Node(6)
+        root.right.right = lca.Node(7)
 
 
-    assert (lca.findLCA(root, 6, 7)) == -1, "Should be -1"
-    return
+        assert (lca.findLCA(root, 6, 7)) == 3, "Should be 3"
+        assert (lca.findLCA(root, 3, 2)) == 1, "Should be 1"
+        assert (lca.findLCA(root, 4, 5)) == 2, "Should be 2"
+        return
 
-testEmpty()
-testOneMissing
-testNormal
+    def testOneMissing(self):
+        root = lca.Node(1)
+        root.left = lca.Node(2)
+        root.right = lca.Node(3)
+        root.left.left = lca.Node(4)
+        root.left.right = lca.Node(5)
+        root.right.left = lca.Node(6)
+
+
+        assert (lca.findLCA(root, 6, 7)) == -1, "Should be -1"
+        return
